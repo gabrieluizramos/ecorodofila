c75b17a029bc569f6b6eada935c6e8772e5e98d2
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-10-21 20:53:31 -0300

    fix auth middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,7 +3,7 @@ const { tokens } = require('../../config/auth');
 const isValidToken = token => tokens.has(token);
 
 const isAuthenticatedUser = (req, res, next) => {
-  if(!isValidToken(req.headers.authorization)) res.status(401).json({ message: 'User not authorized' });
+  if(!isValidToken(req.headers.authorization)) return res.status(401).json({ message: 'User not authorized' });
 
   return next();
 };
