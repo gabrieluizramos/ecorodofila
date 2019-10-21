6a6f8667fa7b66a11509de0bfcafd5e38de65303
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-10-21 18:41:10 -0300

    create incident consumers

---
+++ consumers/incidents/index.js
@@ -0,0 +1,21 @@
+const queue = require('../../config/queues');
+const { connect } = require('amqplib');
+
+const handleQueueMessage = channel => async (message, msg = JSON.parse(message.content.toString())) => {
+  console.log('Message received: ', msg)
+  await channel.ack(message);
+};
+
+module.exports = consume = (async () => {
+  try {
+    const connection = await connect(queue.host);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue.name, queue.assert);
+    await channel.prefetch(1);
+    await channel.consume(queue.name, handleQueueMessage(channel), queue.consume);
+
+  } catch (err) {
+    console.error(err);
+  }
+})();
