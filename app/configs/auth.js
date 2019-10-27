12a441f62380444ef14ef238bc012fdab5c04193
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-10-27 19:14:25 -0300

    chore(config): update auth config with names

--- app/configs/auth.js
+++ app/configs/auth.js
@@ -1,19 +1,18 @@
 const users = new Map([
-  [
-    'user', 'password'
-  ],
-  [
-    'admin', 'admin'
-  ]
+  ['user', 'password'],
+  ['admin', 'admin']
 ]);
 
 const tokens = new Map([
-  [
-    'admin', '123token'
-  ]
+  ['admin', '123token']
+]);
+
+const names = new Map([
+  ['admin', 'Gabriel Ramos']
 ]);
 
 module.exports = {
   tokens,
-  users
+  users,
+  names
 };
