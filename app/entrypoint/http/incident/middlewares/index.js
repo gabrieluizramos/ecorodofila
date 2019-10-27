5299e6b9bca8f38f33ce9c773f0a0220368e795f
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-10-27 01:22:19 -0300

    chore(server): insert cors middleware at server

--- app/entrypoint/http/incident/middlewares/index.js
+++ app/entrypoint/http/incident/middlewares/index.js
@@ -1,10 +1,13 @@
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const auth = require('./auth');
 
 const error = require('./error');
 
 module.exports = {
   default: [
+    cors(),
     bodyParser.json(),
     auth
   ],
